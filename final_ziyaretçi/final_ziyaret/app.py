e1859e6594f83f203e36f897dc3f72841d216be2
lhilaldeniz <hilaldeniz941@gmail.com>
2025-05-24 13:41:35 +0300

    Update app.py

--- final_ziyaretçi/final_ziyaret/app.py
+++ final_ziyaretçi/final_ziyaret/app.py
@@ -247,10 +247,7 @@ def randevu_liste():
     randevular = Randevu.query.all()
     return render_template("randevu_liste.html", randevular=randevular)
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+
 #if __name__ == "__main__":
  #   with app.app_context():
 #        db.create_all()
