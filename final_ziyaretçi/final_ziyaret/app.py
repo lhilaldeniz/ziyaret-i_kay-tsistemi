ab2a9c84d81b44a2593574a45b4c29a9704bfa8e
lhilaldeniz <hilaldeniz941@gmail.com>
2025-05-25 19:14:45 +0300

    Update app.py

--- final_ziyaretçi/final_ziyaret/app.py
+++ final_ziyaretçi/final_ziyaret/app.py
@@ -248,11 +248,6 @@ def randevu_liste():
     return render_template("randevu_liste.html", randevular=randevular)
 
 
-#if __name__ == "__main__":
- #   with app.app_context():
-#        db.create_all()
- #   app.run(debug=True)
-
 import os
 if _name_== "_main_" :
     app.run(host="0.0.0.0 , port=int(os.environ.get("PORT ,5000)))
